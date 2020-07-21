0d53ccdd1a7965d0057abe56f4023f3de6acc4da
Xingwang Liao <kuoruan@gmail.com>
2020-07-21 15:08:30 +0800

    feat: add fetch utils

---
+++ src/fetch/index.ts
@@ -0,0 +1,99 @@
+import HttpError from "./HttpError";
+import {
+  CONTENT_TYPE_JSON,
+  CONTENT_TYPE_TEXT,
+  CONTENT_TYPE_STREAM,
+  HEADER_CONTENT_TYPE,
+} from "./types";
+
+export function requestData<T = any>(
+  url = "",
+  options?: RequestInit,
+  body?: object
+): Promise<T> {
+  if (typeof options === "undefined") {
+    options = {
+      method: "GET",
+      headers: {
+        [HEADER_CONTENT_TYPE]: CONTENT_TYPE_JSON,
+      },
+    };
+  } else if (typeof options === "string") {
+    options = {
+      method: options,
+      headers: {
+        [HEADER_CONTENT_TYPE]: CONTENT_TYPE_JSON,
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    };
+  } else {
+    const headers: HeadersInit = Object.assign(
+      {},
+      {
+        [HEADER_CONTENT_TYPE]: CONTENT_TYPE_JSON,
+      },
+      options.headers ?? {}
+    );
+
+    options = Object.assign(
+      {},
+      {
+        method: "GET",
+      },
+      options,
+      {
+        headers: headers,
+        body: body
+          ? JSON.stringify(body)
+          : options.body
+          ? options.body
+          : undefined,
+      }
+    );
+  }
+
+  return fetch(url, options)
+    .then((res) => {
+      const status = res.status;
+
+      if (status >= 200 && status < 300) {
+        return res;
+      } else {
+        // handle response error
+        return res.text().then((text) => {
+          let message, resObj;
+          try {
+            resObj = JSON.parse(text);
+            message = resObj.message;
+          } catch {
+            message = text;
+            resObj = text;
+          }
+
+          throw new HttpError(status, message, resObj);
+        });
+      }
+    })
+    .then((res) => {
+      let contentType = res.headers.get(HEADER_CONTENT_TYPE);
+      if (contentType) {
+        // handle respose ContentType like 'application/json; charset=utf-8'
+        contentType = contentType.split(";").shift() || "";
+      }
+
+      switch (contentType) {
+        case CONTENT_TYPE_JSON: {
+          return res.json();
+        }
+        case CONTENT_TYPE_TEXT: {
+          return res.text();
+        }
+        case CONTENT_TYPE_STREAM:
+        default: {
+          return res.blob();
+        }
+      }
+    });
+}
+
+export * from "./types";
---
+++ src/fetch/types.ts
@@ -0,0 +1,7 @@
+export const HEADER_CONTENT_TYPE = "Content-Type";
+
+export const CONTENT_TYPE_JSON = "application/json";
+
+export const CONTENT_TYPE_STREAM = "application/octet-stream";
+
+export const CONTENT_TYPE_TEXT = "text/plain";
