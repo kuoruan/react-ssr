868bf112a037eb42eb43ac2d13ffcac68df7cc6a
Xingwang Liao <kuoruan@gmail.com>
2020-12-07 13:08:08 +0800

    feat: add stylelint-config-standard

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: ["stylelint-prettier", "stylelint-order"],
   extends: [
-    "stylelint-config-recommended",
+    "stylelint-config-standard",
     "stylelint-prettier/recommended",
     "stylelint-config-property-sort-order-smacss",
   ],
